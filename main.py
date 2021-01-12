bf633af8e311f642a9cb7e668e2b7569b69f96e2
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 15:26:58 -0500

    Trying to load assests

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import pygame
 import settings
 
 pygame.init()
+
+# SOME INITIALIZATION OF OUR FRAME
 size = (settings.WIDTH, settings.HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Platformer")
  
 run = True
- 
+
+# SETTING A BACKGROUND
+
+bg_surface = pygame.image.load("assets/back.png")
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
@@ -17,8 +23,9 @@ while run:
         if event.type == pygame.QUIT: # If user clicked close
             run = False # Flag that we are done so we exit this loop
   
-    screen.fill(settings.BLACK)
- 
+    screen.blit(bg_surface, (0,0))
+
     # --- Limit to 60 frames per second
     clock.tick(settings.FPS)
+
 pygame.quit()
