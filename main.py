6203f8d2c1bdaf87f6893a59e46313322ffe3676
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 10:54:38 -0500

    Setting up environment

--- main.py
+++ main.py
@@ -1,51 +1,24 @@
-""" 
-A basic pygame template
-"""
- 
 import pygame
- 
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
+import settings
+
 pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (700, 500)
+size = (settings.WIDTH, settings.HEIGHT)
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Platformer")
  
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
-done = False
+run = True
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
+while run:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
+            run = False # Flag that we are done so we exit this loop
   
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+    screen.fill(settings.BLACK)
  
     # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
+    clock.tick(settings.FPS)
 pygame.quit()
