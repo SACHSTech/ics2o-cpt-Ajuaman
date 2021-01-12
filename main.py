d320cc38f4d4e117f90ff9b56c3f611d7b44a999
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 15:44:08 -0500

    Added some online sprites and screen movement

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
-import settings
+import settings as s
 
 pygame.init()
 
 # SOME INITIALIZATION OF OUR FRAME
-size = (settings.WIDTH, settings.HEIGHT)
+size = (s.WIDTH, s.HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Platformer")
  
@@ -12,20 +12,29 @@ run = True
 
 # SETTING A BACKGROUND
 
-bg_surface = pygame.image.load("assets/back.png")
+bg_surface = pygame.image.load("assets/back.png").convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load("assets/base.png")
+floor_surface = pygame.transform.scale2x(floor_surface)
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
+# screen.fill(settings.WHITE)
+# screen.blit(bg_surface, (0,0))
+
 # -------- Main Program Loop -----------
 while run:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             run = False # Flag that we are done so we exit this loop
-  
+
     screen.blit(bg_surface, (0,0))
 
-    # --- Limit to 60 frames per second
-    clock.tick(settings.FPS)
-
+    s.floorX -=1
+    screen.blit(floor_surface, (s.floorX, s.floorY))
+    pygame.display.update()
+     # --- Limit to 60 frames per second
+    clock.tick(s.FPS)
 pygame.quit()
