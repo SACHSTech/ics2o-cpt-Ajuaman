d32cb3c51b526d6f0b4e2821d7513c57fdb932ce
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 19:50:33 -0500

    Adding finishing touches to the game

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-  
+
+'''
+-------------------------------------------------------------------------------
+Name:		main.py
+Purpose:	Main game functionality goes in here
+Author:	Chin.T
+Created: 010/1/2021
+------------------------------------------------------------------------------
+'''
+
 import pygame
 import settings as s
 import time
 import random
 from random import randint
+import math
 
 pygame.init()
 pygame.font.init()
+
+# GENERAL VARIABLES
 collide_counter = 0
 life_to_subtract = 0
 life_left = 50000
+run = True
+transparent = (0, 0, 0, 0)
 
 def floor_looper():
     screen.blit(floor_surface, (s.floorX, 900))
@@ -30,22 +44,29 @@ def draw_virus(viruses):
     for virus in viruses:
         screen.blit(virus_surface, virus)
 
+def calculate_healthbar_length(hp_remaining):
+    return math.floor(((200 * hp_remaining)/50000))
+
 # SOME INITIALIZATION OF OUR FRAME
 size = (s.WIDTH, s.HEIGHT)
 screen = pygame.display.set_mode(size)
+
+# TITLE
 pygame.display.set_caption("(Kinda) Fake Flappy Bird")
 
+# SETTING THE PROGRAM LOGO TO THE BIRD
 programIcon = pygame.image.load('assets/bird.png')
 
 pygame.display.set_icon(programIcon)
 
+# DEALING WITH FONTS AND SIZES 
 font = pygame.font.SysFont('Comic Sans MS', 20)
 troll_font_surface = font.render('haha jk no flabby birb', False, (0, 0, 0))
-dodge_font_surface = font.render('dodge da viruses! they are computer malware and they hurt!!!', False, (0, 0, 0))
+dodge_font_surface1 = font.render('dodge da viruses!', False, (0, 0, 0))
+dodge_font_surface2 = font.render('they are computer malware and they hurt!!!', False, (0, 0, 0))
 lives_text = font.render("Life left:", False, (0, 0, 0))
 
-run = True
-transparent = (0, 0, 0, 0)
+
 # SETTING A BACKGROUND
 bg_surface = pygame.image.load("assets/back.png").convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -64,19 +85,18 @@ virus_surface = pygame.image.load('assets/virus man.png')
 virus_surface = pygame.transform.scale(virus_surface, (130, 130))
 virus_list = []
 
-# LIFE BAR
-life_bar = pygame.Rect(110, 977, 200, 20)
+endgame_surface = pygame.image.load('assets/endgame.png')
+endgame_surface = pygame.transform.scale2x(endgame_surface)
 
 SPAWNVIRUS = pygame.USEREVENT 
-
-# SPAWN A VIRUS OBSTACLE EVERY 500 MILLISECOND
-pygame.time.set_timer(SPAWNVIRUS, 500)
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
 # FLAPPY SCREEN DETERMINES WHAT SCREEN YOU ARE ON. IF YOU ARE ON STARTUP SCREEN FLAPPY SCRREN WILL BE EQUAL TO 0. OTHERWISE, 1
 flappy_screen = 0
+
+pygame.time.set_timer(SPAWNVIRUS, 500)
 # -------- Main Program Loop -----------
 while run:
     for event in pygame.event.get(): # User did something
@@ -84,7 +104,10 @@ while run:
             run = False # Flag that we are done so we exit this loop
         if event.type == SPAWNVIRUS:
             virus_list.append(create_virus())
-             
+    
+    # LIFE BAR LOGIC
+    life_bar = pygame.Rect(110, 977, calculate_healthbar_length(life_left), 20)
+
     # GET KEY PRESSED FROM USER. STORE IN KEYS VARIABLE
     keys = pygame.key.get_pressed()
     # IF A CONTROL BUTTON IS PRESSED THE STARTUP SCREEN DISAPPEARS
@@ -93,31 +116,41 @@ while run:
 
     if keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] and flappy_screen == 0:
         game_over_surface.fill(transparent)
-        screen.blit(troll_font_surface,(0,0))
-        screen.blit(dodge_font_surface,(0, 50))
-        flappy_screen = 1
-        
 
+        # ADDS THE TEXT ON SCREEN
+        screen.blit(troll_font_surface,(0,0))
+        screen.blit(dodge_font_surface1,(0, 50))
+        screen.blit(dodge_font_surface2,(0, 100))
+
+        # CHANGING THE VALUE OF flappy_screen
+        flappy_screen = 1
+
+    # LOGIC FOR DETERMINING PLAYER MOVEMENTS
     if keys[pygame.K_UP]:
         s.playerY -= s.player_jump_height
+        # CREATING BOUNDARIES
         if s.playerY < 0:
             s.playerY = 0
 
     elif keys[pygame.K_DOWN]:
         s.playerY += s.player_jump_height
+         # CREATING BOUNDARIES
         if s.playerY + 186 > s.HEIGHT:
             s.playerY = s.HEIGHT - 186 # 186 is just an arbitrary number that looks best in my opinion. No calculation.
             
     elif keys[pygame.K_RIGHT]:
         s.playerX += s.player_jump_height
+         # CREATING BOUNDARIES
         if s.playerX + 68 > s.WIDTH:
             s.playerX = s.WIDTH - 68 # 68 is just an arbitrary number that looks best in my opinion. No calculation.
 
     elif keys[pygame.K_LEFT]:
         s.playerX -= s.player_jump_height
+         # CREATING BOUNDARIES
         if s.playerX < 0:
             s.playerX = 0
 
+    # MOVING THE PLAYER'S RECTANGLE COLLIDER WITH THE PLAYER
     player_rect.centerx = s.playerX
     player_rect.centery = s.playerY
 
@@ -136,7 +169,7 @@ while run:
     # DRAWING THE LIVES ICON
     
     screen.blit(lives_text,(10, 970))
-    pygame.draw.rect(screen, s.GREEN, life_bar)
+    
     if flappy_screen > 0:
         # VIRUSES
         virus_list = move_virus(virus_list)
@@ -145,11 +178,29 @@ while run:
             if player_rect.colliderect(virus):
                 life_to_subtract += 1
                 life_left -= life_to_subtract
-                print(life_left)
+                print(calculate_healthbar_length(life_left))
+            pygame.draw.rect(screen, s.GREEN, life_bar)
 
+    # GAME LOGIC HERE
+    if calculate_healthbar_length(life_left) <= 0:
+        screen.blit(bg_surface, (0,0))
+        screen.blit(endgame_surface, (100, 412))
+        screen.blit(floor_surface, (s.floorX, s.floorY))
+
+        # LOOP THE FLOOR AGAIN
+        s.floorX -= 2
+        floor_looper()
+
+        # RESETING THE FLOORX POSITION ONCE IT REACHES A CERTAIN POSITION
+        if s.floorX <= -576:
+            s.floorX = 0
+
+    # UPDATING THE SCREEN
     pygame.display.update()
+
      # --- Limit to 60 frames per second
     clock.tick(s.FPS)
+
 
 
 
